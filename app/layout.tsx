73a30a1db3d811412583142041cd92fc9808d905
NDTruongDanh <23520242@gm.uit.edu.vn>
2025-11-18 23:16:36 +0700

    feat: Remove favicon link from Head component in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
