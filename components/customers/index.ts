af37cc08c947f5fd5e1cbe283df38b67b93518c5
NDTruongDanh <23520242@gm.uit.edu.vn>
2025-11-19 14:13:15 +0700

    Revert "created customer management page"
    This reverts commit c6b26ae6beda5386a3f84a7118730bfcc11b66fc.

--- components/customers/index.ts
+++
@@ -1,6 +0,0 @@
-export { CustomerTable } from "./customer-table";
-export { CustomerFormModal } from "./customer-form-modal";
-export { CustomerDetailModal } from "./customer-detail-modal";
-export { CustomerFilters } from "./customer-filters";
-export { CustomerStatisticsCards } from "./customer-statistics-cards";
-export { CustomerDeleteDialog } from "./customer-delete-dialog";
